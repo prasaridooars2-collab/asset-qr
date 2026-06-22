034164dccf6d7eb1dc089a16202ae4915fd8e8e5
prasaridooars2-collab <prasaridooars2@gmail.com>
2026-06-22 09:17:26 +0530

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,22 +1,11 @@
 <?php
 
-$conn = mysqli_init();
+return [
+    'host' => 'mysql-2482696a-prasari.l.aivencloud.com',
+    'port' => 19125,
+    'name' => 'defaultdb',
+    'user' => 'avnadmin',
 
-mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
-
-mysqli_real_connect(
-    $conn,
-    getenv("DB_HOST"),
-    getenv("DB_USER"),
-    getenv("DB_PASS"),
-    getenv("DB_NAME"),
-    (int)getenv("DB_PORT"),
-    NULL,
-    MYSQLI_CLIENT_SSL
-);
-
-if (!$conn) {
-    die("Database connection failed: " . mysqli_connect_error());
-}
-
-?>
+    // Render Environment Variable থেকে নেবে
+    'pass' => getenv('DB_PASSWORD')
+];
